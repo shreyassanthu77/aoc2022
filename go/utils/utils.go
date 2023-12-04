9370c76728ca076282a637b77b5c629d88442496
Shreyas Mididoddi <me@shreyascodes.tech>
2023-12-04 18:38:17 +0530

    go: fix utils

--- go/utils/utils.go
+++ go/utils/utils.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func ReadInput(day int) string {
-	fileName := fmt.Sprintf("day%02d/input.txt", day)
+	fileName := fmt.Sprintf("../input/day%02d.txt", day)
 
 	file, err := os.ReadFile(fileName)
 	if err != nil {
-		log.Fatal("Error reading input file", fileName)
+		log.Fatal("Error reading input file: ", fileName)
 	}
 
 	return string(file)
+}
+
+func ReadInputLines(day int) []string {
+	file := ReadInput(day)
+	return strings.Split(file, "\n")
 }
